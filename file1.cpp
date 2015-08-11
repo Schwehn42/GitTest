2069d376441b6c253cf710fa509dea1fa6a1d571
Jakob Schwehn <schwehn42@gmail.com>
2015-08-11 19:19:13 +0200

    balanced, totally doesnt include bug

--- file1.cpp
+++ file1.cpp
@@ -2,6 +2,17 @@
 
 int main() {
 	using std;
-	std::cout << "ayyyy" << std:endl;
+	std::cout << "ayy" << std:endl;
 	std::cout << "lmao";
+
+
+	a
+	a
+	sdsd
+	d
+
+	do {
+		/* code */
+	} while(d
+	d);
 }
